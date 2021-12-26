723af44dff48430092d4c1d67acfdbed086b80dd
Anthony Costarelli <acostarelli@olin.edu>
2021-12-26 18:47:23 -0500

    oh yeah baby

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, choices
 
 from mido import MidiFile, MidiTrack, Message
 
@@ -6,20 +6,50 @@ from music import Rhythm, Event, Note
 from standards import standards
 from licks import licks
 
+from pprint import pprint
+
 def v1random(standard):
+    """
+    Chooses licks based on the previous note and the first note of the lick.
+    """
+    weights = [
+        0, # unison
+        4, # m2
+        2, # M2
+        2, # m3
+        2, # M3
+        0, # P4
+        0, # d5
+        0, # P5
+        0, # m6
+        0, # M6
+        0, # m7
+        0  # M7
+    ]
+    cvt = [0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
+
     key = 60
     last = None
     for chord in standard:
-        lick = choice([lick for lick in licks if lick.quality == chord.quality])
+        if last and last != Note.REST:
+            lick = choices(
+                [lick for lick in licks if lick.quality == chord.quality],
+                weights=[weights[cvt[abs((last % 12) - (chord.root + lick.first.note) % 12)]] for lick in licks if lick.quality == chord.quality]
+            )[0]
+        else:
+            lick = choice([lick for lick in licks if lick.quality == chord.quality])
+        first = key + chord.root + lick.first.note
 
         correction = 0
         if last and last != Note.REST and lick.first.note != Note.REST:
-            correction = -12 * int(((key + chord.root + lick.first.note) - last) / 12)
+            correction = 6 * int((last - first) / 6)
 
-        for event in lick:
-            last = key + chord.root + event.note + correction
+        first += correction
+        yield Event(first, lick.first.rhythm)
+
+        for event in lick[1:]:
+            last = first + event.note
             yield Event(last, event.rhythm)
-
 
 """
 1. no weird octaves
